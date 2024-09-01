9914e7b6d77289fd57c2627dbe63ef226bdc08f6
huntingforheat <asktt1234@gmail.com>
2024-09-01 12:05:09 +0900

    🚧 등급 수정 완료

--- client/src/(FSD)/entities/manager/ui/UserList.tsx
+++ client/src/(FSD)/entities/manager/ui/UserList.tsx
@@ -1,163 +1,3 @@
-// "use client";
-
-// import { useGetAllUsers, ALL_USERS_QUERY_KEY } from '@/(FSD)/entities/manager/api/useGetAllUsers';
-// import { UserCustomerInfo } from '@/(FSD)/shareds/types/manager/UserCustomerInfo.type';
-// import React, { useCallback, useState } from 'react';
-// import styles from '@/(FSD)/shareds/styles/UserList.module.scss';
-// import { useDeleteUser } from '../api/useDeleteUser';
-// import { useQueryClient } from '@tanstack/react-query';
-// import PointAdjustmentModal from './PointAdjustmentModal';
-
-
-// const UserList = () => {
-//     const queryClient = useQueryClient();
-//     const { data, isLoading, error, refetch } = useGetAllUsers();
-//     const deleteUser = useDeleteUser();
-//     const [selectedUser, setSelectedUser] = useState<UserCustomerInfo | undefined>(undefined);
-//     const [isPointModalOpen, setIsPointModalOpen] = useState(false);
-//     const [isLevelModalOpen, setIsLevelModalOpen] = useState(false);
-//     const [updateTrigger, setUpdateTrigger] = useState(0);
-
-//     if (isLoading) return <div>Loading...</div>;
-//     if (error) return <div>Error: {error.message}</div>;
-
-//     const handleDeleteUser = async (userId: number) => {
-//         if (window.confirm('정말로 이 사용자를 삭제하시겠습니까?')) {
-//             try {
-//                 await deleteUser.mutateAsync(userId);
-//                 alert('사용자가 성공적으로 삭제되었습니다.');
-//                 queryClient.invalidateQueries({ queryKey: [ALL_USERS_QUERY_KEY] });
-//             } catch (error) {
-//                 console.error('사용자 삭제 중 오류 발생: ', error);
-//                 alert('사용자 삭제 중 오류가 발생했습니다.');
-//             }
-//         } else {
-//             alert('관리자가 취소하였습니다.');
-//         }
-//     }
-
-//     const handleModalClose = useCallback(() => {
-//         setIsPointModalOpen(false);
-//         setIsLevelModalOpen(false);
-//         setSelectedUser(undefined);
-//         refetch();
-//       }, [refetch]);
-
-//     const handlePointUpdate = useCallback(() => {
-//         setUpdateTrigger(prev => prev + 1);
-//         handleModalClose();
-//     }, [handleModalClose]);
-
-//     React.useEffect(() => {
-//         refetch();
-//     }, [updateTrigger, refetch]);
-
-//     return (
-//         <div className={styles.tableContainer}>
-//             <table className={styles.userTable}>
-//             {/* <colgroup>
-//                 <col style={{width: '5%'}} />
-//                 <col style={{width: '15%'}} />
-//                 <col style={{width: '15%'}} />
-//                 <col style={{width: '15%'}} />
-//                 <col style={{width: '20%'}} />
-//                 <col style={{width: '5%'}} />
-//                 <col style={{width: '10%'}} />
-//                 <col style={{width: '5%'}} />
-//                 <col style={{width: '10%'}} />
-//             </colgroup> */}
-//                     <thead>
-//                         <tr>
-//                             <th>userID</th>
-//                             <th>Email</th>
-//                             <th>이름</th>
-//                             <th>전화번호</th>
-//                             <th>주소</th>
-//                             <th>권한</th>
-//                             <th>적립금</th>
-//                             <th>등급</th>
-//                             <th>액션</th>
-//                         </tr>
-//                     </thead>
-//                     <tbody>
-//                         {data?.map((user) => (
-//                             <tr key={user.userId}>
-//                                 <td>{user.userId}</td>
-//                                 <td>{user.email}</td>
-//                                 <td>{user.name}</td>
-//                                 <td>{user.phoneNumber}</td>
-//                                 <td>{user.address}</td>
-//                                 <td>{user.roles.join(', ')}</td>
-//                                 {/* <td>{user.point || '-'}</td> */}
-//                                 <td>
-//                                     {user.point || '-'}
-//                                     <button
-//                                         className={styles.actionButton}
-//                                         onClick={() => {
-//                                             setSelectedUser(user);
-//                                             setIsPointModalOpen(true);
-//                                         }}
-//                                     >
-//                                         적립금 수정
-//                                     </button>
-//                             </td>
-//                                 {/* <td>{user.level}</td> */}
-//                                 <td>
-//                                     {user.level}
-//                                     <button
-//                                         className={styles.actionButton}
-//                                         onClick={() => {
-//                                             setSelectedUser(user);
-//                                             setIsLevelModalOpen(true);
-//                                         }}
-//                                     >
-//                                         등급 수정
-//                                     </button>
-//                                 </td>
-//                                 <td>
-//                                     <button
-//                                             className={styles.deleteButton}
-//                                             onClick={() => handleDeleteUser(user.userId)}
-//                                     >
-//                                             삭제
-//                                     </button>
-//                                 </td>
-//                             </tr>
-//                         ))}
-//                     </tbody>
-//             </table>
-//             {selectedUser && (
-//                 <>
-//                     {/* <PointAdjustmentModal
-//                         isOpen={isPointModalOpen}
-//                         onClose={() => {
-//                             setIsPointModalOpen(false);
-//                             setSelectedUser(undefined);
-//                         }}
-//                         user={selectedUser}
-//                     /> */}
-//                     <PointAdjustmentModal
-//                         isOpen={isPointModalOpen}
-//                         onClose={handleModalClose}
-//                         onUpdate={handlePointUpdate}
-//                         user={selectedUser}
-//                     />
-//                     {/* <LevelAdjustmentModal
-//                         isOpen={isLevelModalOpen}
-//                         onClose={() => {
-//                             setIsLevelModalOpen(false);
-//                             setSelectedUser(undefined);
-//                         }}
-//                         user={selectedUser}
-//                     /> */}
-//                 </>
-//             )}
-//         </div>
-//     );
-// };
-
-// export default UserList;
-
 "use client";
 
 import { useGetAllUsers, ALL_USERS_QUERY_KEY } from '@/(FSD)/entities/manager/api/useGetAllUsers';
@@ -167,6 +7,7 @@ import styles from '@/(FSD)/shareds/styles/UserList.module.scss';
 import { useDeleteUser } from '../api/useDeleteUser';
 import { useQueryClient } from '@tanstack/react-query';
 import PointAdjustmentModal from './PointAdjustmentModal';
+import LevelAdjustmentModal from './LevelAdjustmentModal';
 
 const UserList = () => {
     const queryClient = useQueryClient();
@@ -198,15 +39,19 @@ const UserList = () => {
         refetch();
     }, [refetch]);
 
-    const handlePointUpdate = useCallback(() => {
-        handleModalClose();
-    }, [handleModalClose]);
+    // const handlePointUpdate = useCallback(() => {
+    //     handleModalClose();
+    // }, [handleModalClose]);
 
-    useEffect(() => {
-        if (!isLoading && !error) {
-            refetch();
-        }
-    }, [isLoading, error, refetch]);
+    const handleUpdate = useCallback(() => {
+        refetch();
+    }, [refetch]);
+
+    // useEffect(() => {
+    //     if (!isLoading && !error) {
+    //         refetch();
+    //     }
+    // }, [isLoading, error, refetch]);
 
     if (isLoading) return <div>Loading...</div>;
     if (error) return <div>Error: {error.message}</div>;
@@ -273,12 +118,20 @@ const UserList = () => {
                 </tbody>
             </table>
             {selectedUser && (
-                <PointAdjustmentModal
-                    isOpen={isPointModalOpen}
-                    onClose={handleModalClose}
-                    onUpdate={handlePointUpdate}
-                    user={selectedUser}
-                />
+                <>
+                    <PointAdjustmentModal
+                        isOpen={isPointModalOpen}
+                        onClose={handleModalClose}
+                        onUpdate={handleUpdate}
+                        user={selectedUser}
+                    />
+                    <LevelAdjustmentModal
+                        isOpen={isLevelModalOpen}
+                        onClose={handleModalClose}
+                        onUpdate={handleUpdate}
+                        user={selectedUser}
+                    />
+                </>
             )}
         </div>
     );
